2a58589d4172233b0237f17940191f4702c9fd4a
Panda <nhantran.tvda@gmail.com>
2023-04-27 14:36:59 +0700

    update

--- src/palindrome.test.js
+++ src/palindrome.test.js
@@ -1,18 +1,18 @@
 const longestPalSubstr = require("./palindrome");
 
-test('palindrome string is "amanaplanacanalpanama" ', () => {
+test('palindrome string is "amanaplanacanalpanama" length is 21', () => {
   let str = "A man a plan a canal Panama";
-  expect(longestPalSubstr(str)).toEqual("amanaplanacanalpanama");
+  expect(longestPalSubstr(str)).toEqual(21);
 });
-test('palindrome string is "geeksskeeg" ', () => {
+test('palindrome string is "geeksskeeg" length is 10 ', () => {
   let str = "forgeeksskeegfor";
-  expect(longestPalSubstr(str)).toEqual("geeksskeeg");
+  expect(longestPalSubstr(str)).toEqual(10);
 });
-test('palindrome string is "3553" ', () => {
+test('palindrome string is "3553" length is 4 ', () => {
   let str = "3 5 5 3";
-  expect(longestPalSubstr(str)).toEqual("3553");
+  expect(longestPalSubstr(str)).toEqual(4);
 });
-test('palindrome string is "radar" ', () => {
+test('palindrome string is "radar" length is 5', () => {
   let str = "r a d a r";
-  expect(longestPalSubstr(str)).toEqual("radar");
+  expect(longestPalSubstr(str)).toEqual(5);
 });
